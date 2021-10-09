406bae482ca0a4d713f24d5b6f3416f175e949e7
viertelb <7363661+viertelb@users.noreply.github.com>
2021-10-09 09:33:39 +0200

    Fix wrong item number,
    print stack trace, print better output

--- module/qr.py
+++ module/qr.py
@@ -4,33 +4,32 @@ import qrcode.image.pil
 import check_exists as exist
 import cleanup_strings as clean
 import get_collection as collection
+import traceback
 
 
 def create_qr(_db, username, apitoken):
-    print("Create QR codes")
-    print("")
     total_items = collection.get_total_item(username, apitoken)
-    for item in range(0, total_items + 1):
+    print("Going to create {} QR codes:\n".format(total_items))
+    for item in range(0, total_items):
         try:
             discogs_no = str(_db.iloc[item]['discogs_no'])
             artist = str(_db.iloc[item]['artist'])
             album_title = str(_db.iloc[item]['album_title'])
             discogs_link = str(_db.iloc[item]['discogs_webpage'])
-            gen_qr(discogs_link, discogs_no, artist, album_title)
-            print("QR-Code for " + artist + "-" + album_title + " created.")
-        except:
-            print("Unable to create QR code for item: " + str(item) + " with album: " + album_title)
+            gen_qr(discogs_link, discogs_no, artist, album_title, item)
+        except Exception:
+            print("Unable to create QR code for json-array # {} with album {}".format(item, album_title))
+            traceback.print_exc()
 
-    print("All done!")
+    print("All done!\n")
 
 
-def gen_qr(discogs_link, discogs_no, artist, album_title):
+def gen_qr(discogs_link, discogs_no, artist, album_title, item='None'):
     exist.folder_checker("qr")
 
 
     # output filename
     filename = discogs_no + "_" + clean.cleanup_artist_url(artist) + "-" + clean.cleanup_title_url(album_title) + ".png"
-    print("filename: " + filename)
     if not exist.file_checker(filename):
         # Create qr code instance
         qr = QRCode(version=4, box_size=5, border=0, error_correction=ERROR_CORRECT_L)
@@ -40,4 +39,6 @@ def gen_qr(discogs_link, discogs_no, artist, album_title):
         qr.make(fit=True)
         im = qr.make_image(qrcode.image.pil.PilImage)
         im.save("qr/" + filename)
+
+    print("Created QR code # {} for '{}-{}'.".format(item+1, artist, album_title))
 
