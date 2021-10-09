3623ebd562534d2d9db46b56d7aea7727b58beaa
viertelb <7363661+viertelb@users.noreply.github.com>
2021-10-09 09:32:49 +0200

    Remove unnecessary imports
    and avoid redundand code

--- module/write_to_file.py
+++ module/write_to_file.py
@@ -1,23 +1,19 @@
-import pandas as pd
 from pandas import ExcelWriter
-from pandas import ExcelFile
 
-import csv
-import xlsxwriter
 
 
 def write_2_csv(collection, filename):
-    collection.to_csv(filename + '.csv', sep='\t', encoding='utf-8')
-    return True
+    filename = filename + '.csv'
+    collection.to_csv(filename, sep='\t', encoding='utf-8')
+    return filename
 
 
 def write_2_excel(collection, filename):
-    # Write to Excel File
-    writer = ExcelWriter(filename + '.xlsx')
+    filename = filename + '.xlsx'
+    writer = ExcelWriter(filename)
     collection.to_excel(writer, 'Sheet1', index=False)
     writer.save()
-    print("Write to File successful.")
-    return True
+    return filename
 
 def write_file(_db, filetype):
     _db.sort_values(["date_added", "artist"], axis=0,
@@ -26,10 +22,12 @@ def write_file(_db, filetype):
         filename = str(input("Enter Filename of {}-File: ".format(filetype)))
         if filename != "_db":
             if filetype == 'Csv':
-                write_2_csv(_db, filename)
+                filename = write_2_csv(_db, filename)
             elif filetype == 'Excel':
-                write_2_excel(_db, filename)
+                filename = write_2_excel(_db, filename)
             break
 
         else:
-            print("You can't name the file '_db' !")
+            print("You can't name the file '_db' !\n")
+
+    print("Write to file '{}' successful.\n".format(filename))
