de56eaf4ba6dcd1312eee025c7559238326b2f26
blackbunt@posteo.se <$n3qLBaPVZO!a#!5z0%TS8!cPyRdTCqN%zzDsTr7y2FwVpTRP7jrIq@lBDChM4rGN2OM*xj6>
2021-12-29 16:51:32 +0100

    added Globals for config and menu obj

--- main.py
+++ main.py
@@ -3,12 +3,15 @@
 import module
 from module import menu, login
 import os
+import sys
 
 CONFIG_PATH = os.path.join(os.getcwd(), 'config/config.yaml')
-# load general config
+MENU_PATH = os.path.join(os.getcwd(), 'config/menu.yaml')
+# load general config file
 config = menu.read_config(CONFIG_PATH)
-# load menu Configuration
-menus = menu.read_config(CONFIG_PATH)
+# load menu config file
+menus = menu.read_config(MENU_PATH)
 # check if logindata exists
-if login.check_login(config, CONFIG_PATH):
-    print('Connection established.')
+if not login.check_login(config, CONFIG_PATH):
+    sys.exit('Connection to Discogs not possible.\nNo Network Connection?')
+menu.menu_main(menus)
