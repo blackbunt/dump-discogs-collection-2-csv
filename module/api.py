7fcc27fcf228d033fbdedc6d73fcabe4d5b3d5e0
blackbunt@posteo.se <$n3qLBaPVZO!a#!5z0%TS8!cPyRdTCqN%zzDsTr7y2FwVpTRP7jrIq@lBDChM4rGN2OM*xj6>
2022-01-02 17:43:58 +0100

    added kwargs to get_query()

--- module/api.py
+++ module/api.py
@@ -6,7 +6,7 @@ handles api calls
 import requests
 import config
 import os
-
+import json
 
 
 def get_headers(conf: dict):
@@ -19,7 +19,7 @@ def get_headers(conf: dict):
     return headers
 
 
-def get_query(conf: dict):
+def get_query(conf: dict, **kwargs):
     """
     Build the query for the request
     :param conf: config dict
@@ -28,12 +28,21 @@ def get_query(conf: dict):
     token = conf['Login']['apitoken']
     params = conf['API']['params']
 
-    conf_query = conf['API']['params']
-    token_key = list(conf_query.keys())[2]
-    params.pop('token', None)
-    token_entry = {token_key: token}
-    # merge dicts
-    query = params | token_entry
+    if kwargs:
+        page = kwargs.get('page')
+        per_page = conf['API']['limit']
+        query = {
+            'token': token,
+            'page': page,
+            'per_page': per_page
+        }
+    else:
+        conf_query = conf['API']['params']
+        token_key = list(conf_query.keys())[2]
+        params.pop('token', None)
+        token_entry = {token_key: token}
+        # merge dicts
+        query = params | token_entry
     return query
 
 
@@ -57,9 +66,20 @@ def login_api(user: str, token: str, conf: dict):
 
 def get_info_api(conf: dict):
     """
-    Generates a request to dump the collection info of user
+    Generates a request to dump the collection value of user
+    :param conf:
+    :return: json and header info
+    """
+    url = conf['API']['release_url']
+    url = url.replace('{username}', conf['Login']['username'])
+    limit = conf['API']['limit']
+
+
+def get_value_api(conf: dict):
+    """
+    Generates a request to dump the collection value info of user
     :param conf: configfile
-    :return: json data
+    :return: json
     """
     url = conf['API']['value_url']
     url = url.replace('{username}', conf['Login']['username'])
@@ -70,16 +90,17 @@ def get_info_api(conf: dict):
         headers=get_headers(conf),
     )
     if res.status_code == 200:
-        pass
+        return json.loads(res.text.encode('utf-8'))
     else:
         raise ConnectionError
+
 
 if __name__ == '__main__':
     root_dir = os.getcwd()
     root_dir = os.path.dirname(root_dir)
     configfile = config.read_config(os.path.join(root_dir, 'config/config.yaml'))
     # noinspection PyUnresolvedReferences
-    username = config['Login']['username']
+    username = configfile['Login']['username']
     # noinspection PyUnresolvedReferences
-    token = config['Login']['apitoken']
-    print(login_api(configfile))
+    token = configfile['Login']['apitoken']
+    print(get_value_api(configfile))
