ebb25814bc8556c21f7b1b3d1e2d1cbfef023405
blackbunt@posteo.se <$n3qLBaPVZO!a#!5z0%TS8!cPyRdTCqN%zzDsTr7y2FwVpTRP7jrIq@lBDChM4rGN2OM*xj6>
2022-01-02 19:15:55 +0100

    show_info() -> setup_value_data()

--- module/collection.py
+++ module/collection.py
@@ -8,7 +8,25 @@ import config
 import login
 import api
 
-def show_info(config_yaml: dict):
+
+def setup_value_data(config_yaml: dict):
+    """
+    Returns a list which contains all the neccesary information for 'Show Library Statistics Menu'
+    :param config_yaml:
+    :return: list
+    """
+    info = {}
+    liste = []
+    req = api.get_collection_info(config_yaml)
+    info['Username'] = config_yaml['Login']['username']
+    info['Total items'] = req['pagination']['items']
+    res = api.get_value_api(config_yaml)
+    info['Min. Value'] = res['minimum']
+    info['Med. Value'] = res['median']
+    info['Max. Value'] = res['maximum']
+    for key, value in info.items():
+        liste.append(f'{key}: {value}')
+    return liste
 
 
 
@@ -17,4 +35,5 @@ if __name__ == '__main__':
     root_dir = os.getcwd()
     root_dir = os.path.dirname(root_dir)
     configfile = config.read_config(os.path.join(root_dir, 'config/config.yaml'))
-    print(login.check_login(configfile, os.path.join(root_dir, 'config/config.yaml')))
+    infos = setup_value_data(configfile)
+    print(infos)
