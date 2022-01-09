233a7aafe547717a88fd6c60de5d546a3d987ba6
blackbunt@posteo.se <$n3qLBaPVZO!a#!5z0%TS8!cPyRdTCqN%zzDsTr7y2FwVpTRP7jrIq@lBDChM4rGN2OM*xj6>
2022-01-09 17:33:08 +0100

    working formula

--- module/benchmark.py
+++ module/benchmark.py
@@ -19,69 +19,37 @@ def benchmark_per_page(config_yaml: str):
     """
     # get no of items in collection
     #no = api.get_info_api(config_yaml)['pagination']['items']
-    no = 4534
-    # if collection size exceeds Api Limit than return 100
-    if no > 6000:
-        return [100]
-    else:
+    no = 2421
 
-        # divider
-        div = 100
-        liste = []
+
+    # upper treshold
+    upper = 100
+    # lower treshold
+    lower = math.ceil(no / 60)
+    liste = []
+    # if collection size exceeds Api Limit than return 100
+    if lower >= upper:
+        return [100]
+        #print(f'{no} equal -> 100')
+    else:
+        div = upper
         handler = True
         while handler:
             mod = no % div
             res = no / div
-            if 0 <= mod <= 9 and res <= 100:
+            if 0 <= mod <= lower and res <= 100:
                 res = math.ceil(res)
                 if not res * 60 < no:
                     if not res > 100:
-                        print(res, mod)
+                        # print(res, mod)
                         liste.append(res)
-            if div == 1:
+            if div == lower:
                 break
             div -= 1
         if not len(liste):
-            liste.append(100)
-        return liste
+            liste.append(lower)
 
-
-def test (config_yaml: str):
-    """
-    Returns possible per_page settings for api call without exceeding api limit. Only works with collections smaller than 6000 items.
-
-    :param config_yaml: list
-    :return:
-    """
-    # get no of items in collection
-    for no in range(6000, 1, -1):
-
-        # if collection size exceeds Api Limit than return 100
-        if no > 6000:
-            return [100]
-        else:
-
-            # divider
-            div = 100
-            liste = []
-            handler = True
-            while handler:
-                mod = no % div
-                res = no / div
-                if 0 <= mod <= 9 and res <= 100:
-                    res = math.ceil(res)
-                    if not res * 60 < no:
-                        if not res > 100:
-                            #print(res, mod)
-                            liste.append(res)
-                if div == 1:
-                    break
-                div -= 1
-            if not len(liste):
-                print(no)
-
-
-
+    return liste
 
 
 if __name__ == '__main__':
@@ -90,4 +58,5 @@ if __name__ == '__main__':
     configfile = config.read_config(os.path.join(root_dir, 'config/config.yaml'))
     #print(max(cpu_count() - 1, 1))
     print(benchmark_per_page(configfile))
-    #test(configfile)
+    #res = test(configfile)
+    #print(res)
